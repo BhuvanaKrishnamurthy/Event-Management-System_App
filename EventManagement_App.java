bb43f222466c28ecb63411f547cbcc148340b7fd
BhuvanaKrishnamurthy <155227135+BhuvanaKrishnamurthy@users.noreply.github.com>
2025-08-22 11:58:22 +0530

    Add files via upload

---
+++ EventManagement_App.java
@@ -0,0 +1,13 @@
+package jsp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagement_App{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagement_App.class, args);
+	}
+
+}
